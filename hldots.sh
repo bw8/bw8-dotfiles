044fff2f7955fc62bb8f68de498aef029a810ba6
bw8 <>
2021-07-23 02:35:17 +0200

    remove file if not hardlinked

--- hldots.sh
+++ hldots.sh
@@ -5,4 +5,12 @@
 #   - Create directory with the name of directory to hardlink inside config
 #   - Run script !
 
-find config/* -maxdepth 0 -type d | xargs -I{} cp -vurl $HOME/.{} config
+# Remove if file is not a hardlinked config file
+find config -type f -links -2 | xargs --no-run-if-empty -o -I{} rm -i {}
+
+# Get list of directories and hardlink them
+find config/* -maxdepth 0 -type d | xargs -r -I{} cp -vurl $HOME/.{} config
+
+rm -r config/Xresources/{rofi,xclock,xfiles}
+rm -r config/mpv/watch_later
+rm -r config/zathura/zathurarc.bak
